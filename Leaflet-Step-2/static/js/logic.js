2965571754d1dd077145052b33ed82f18f9c1062
hvinoy <vinoyharsha@gmail.com>
2021-07-16 00:42:37 -0400

    updates

--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -1,6 +1,64 @@
-function createMap(earthquake) {
+//Create a map object
+// var myMap = L.map("map", {
+//   center: [37.09, -95.71],
+//   zoom: 5
+// });
 
-  // Define streetmap and darkmap layers
+// L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//   tileSize: 512,
+//   maxZoom: 18,
+//   zoomOffset: -1,
+//   id:"mapbox/light-v10",
+//   accessToken: API_KEY
+// }).addTo(myMap);
+
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+d3.json(url).then(function(data) {
+  // console.log(data);
+  var circlemarkers = [];
+  for (var i = 0; i< data.features.length; i++) {
+    var feature = data.features[i];
+    var depth = feature.geometry.coordinates[2];
+    // console.log(depth)
+    var mag = feature.properties.mag;
+    // console.log(mag)
+    var latlng = feature.geometry.coordinates
+    var color = "";
+    if (depth >= 90) {
+      color = "#FF0D0D";
+    }
+    else if (depth < 90 && depth >=  70){
+      color = " #FF4E11";
+    }
+    else if (depth < 70 && depth >= 50) {
+      color = "#FF8E15";
+    }
+    else if (depth <50 && depth  >= 30){
+      color = "#FAB733";
+    }
+    else if (depth < 30 && depth >= 10) {
+      color = "#ACB334";
+    }
+    else {
+      color = "#69B34C";
+    }
+    circlemarkers.push(
+      L.circleMarker([latlng[1], latlng[0]], {
+          fillOpacity:0.75,
+          color:"black",
+          weight: 0.5,
+          fillColor:color,
+          radius: mag * 5
+        }).bindPopup("<h3>" + feature.properties.place +"<h3><hr><p>" + new Date(feature.properties.time) + "</p><hr><p> Magnitude: " 
+        + feature.properties.mag + " & Depth: " + depth+ "</p>")
+        );
+      }
+
+
+var earthquakes = L.layerGroup(circlemarkers);
+
   var greymap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -32,7 +90,7 @@ function createMap(earthquake) {
 
   // Create overlay object to hold our overlay layer
   var overlayMaps = {
-    Earthquakes: earthquake
+    Earthquakes: earthquakes
   };
 
   // Create our map, giving it the greyscale and earthquakes layers to display on load
@@ -41,8 +99,10 @@ function createMap(earthquake) {
       37.09, -95.71
     ],
     zoom: 5,
-    layers: [greymap, earthquake]
+    layers: [greymap, earthquakes]
   });
+
+
 
   // Create a layer control
   // Pass in our baseMaps and overlayMaps
@@ -51,159 +111,23 @@ function createMap(earthquake) {
     collapsed: false
   }).addTo(myMap);
 
-  
-//Create a map object
-// var myMap = L.map("map", {
-//     center: [37.09, -95.71],
-//     zoom: 5
-//   });
-  
-//   L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-//     tileSize: 512,
-//     maxZoom: 18,
-//     zoomOffset: -1,
-//     id:"mapbox/light-v10",
-//     accessToken: API_KEY
-//   }).addTo(myMap);
-
-  var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-
-  // d3.json(url).then(function(data) {
-  //   console.log(data);
-  //     var feat = data.features
-  
-  //     var circlemarkers = []
-  //     for (var i = 0; i< feat.length; i++) {
-  //       var loc = feat[i].geometry;
-  //       var property = feat[i].properties;
-  //       console.log(loc) 
-
-  //       circlemarkers.push(
-  //         L.circleMarker([loc.coordinates[1], loc.coordinates[0]], {
-  //           fillOpacity:0.75,
-  //           color:"black",
-  //           weight: 1,
-  //           fillColor:"green",
-  //           radius: property.mag * 5
-  //         }).bindPopup("<h3>" + property.place +"<h3><hr><p>" + new Date(property.time) + "</p>").addTo(myMap));
-  //       }
-  // })
-
-
-
-d3.json(url).then(function(response){
-  createFeatures(response.features);
-});
-
-function createFeatures(data) {
-  function onEachFeature (feature,layer) {
-    var magnitude = feature.properties.mag
-    var depth = feature.geometry.coordinates[2]
-    var color = ""
-    if (depth >= 90) {
-      color = "#FF0D0D";
-    }
-    else if (depth < 90 && depth >=  70){
-      color = " #FF4E11";
-    }
-    else if (depth < 70 && depth >= 50) {
-      color = "#FF8E15";
-    }
-    else if (depth <50 && depth  >= 30){
-      color = "#FAB733";
-    }
-    else if (depth < 30 && depth >= 10) {
-      color = "#ACB334";
-    }
-    else {
-      color = "#69B34C";
-    }
-
-    var circlemarkers = []
-    circlemarkers.push(
-      L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
-        fillOpacity:0.75,
-        color:"black",
-        weight: 0.5,
-        fillColor:color,
-        radius: magnitude * 5
-      }).bindPopup("<h3>" + feature.properties.place +"<h3><hr><p>" + new Date(feature.properties.time) + "</p><hr><p> Magnitude: " + feature.properties.mag+"</p>").addTo(myMap)); 
-
-  //     return L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
-  //       fillOpacity:0.75,
-  //       color:"black",
-  //       weight: 0.5,
-  //       fillColor:color,
-  //       radius: magnitude * 5
-  //     }).bindPopup("<h3>" + feature.properties.place +"<h3><hr><p>" + new Date(feature.properties.time) + "</p><hr><p> Magnitude: " + feature.properties.mag+"</p>");
-
-  // }
-
-    // var circlemarkers = []
-    // circlemarkers.push(
-    //   L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
-    //     fillOpacity:0.75,
-    //     color:"black",
-    //     weight: 0.5,
-    //     fillColor:color,
-    //     radius: magnitude * 5
-    //   }).bindPopup("<h3>" + feature.properties.place +"<h3><hr><p>" + new Date(feature.properties.time) + "</p><hr><p> Magnitude: " + feature.properties.mag+"</p>"));
-
-  }
-
-  var earthquake = L.geoJSON(data, {
-    onEachFeature: onEachFeature
-  });
-
-  createMap(earthquake); 
-};
-
-
 
   var legend = L.control({position: "bottomright"});
   legend.onAdd = function(){
+  var div = L.DomUtil.create ("div", "info legend");
+  
+  
+  var depths = [-10, 10, 30, 50, 70, 90];
   var colorscale = ["#69B34C","#ACB334","#FAB733","#FF8E15"," #FF4E11","#FF0D0D"];
-  var div = L.DomUtil.create('div', 'info legend'),
-      grades = [-10, 10, 30, 50, 70, 90],
-      labels = [];
-
-  for (var i = 0; i <grades.length; i++) {
-    div.innterHTML +=
-      '<i style="background:' + colorscale[i] + ' "></i>' +
-      grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-
+  
+  for (var i = 0; i <depths.length; i++) {
+    div.innerHTML += 
+    "<li style=\"background-color: " + colorscale[i]+ "\">" + depths[i] + (depths[i+1] ? "&ndash;" + depths[i+1] + "<br>" : "+" + "</li>");
+    //"<ul class =\"no-bullets\">" + "<li style=\"background-color: " + colorscale[i]+ "\"></li>" + depths[i] + (depths[i+1] ? "&ndash;" + depths[i+1] + "<br>" : "+" + "</ul>");
   }
   return div;
-};
+  };
+  legend.addTo(myMap);   
 
-legend.addTo(myMap);
-}
-  
-// var legend = L.control({position: "bottomright"});
-// legend.onAdd = function(){
-//   var div = L.DomUtil.create ("div", "info legend");
-//   var colorscale = ["#FF0D0D"," #FF4E11","#FF8E15","#FAB733","#ACB334","#69B34C"]
-//   var depths = [90,70,50,30,10,-10]
-
-//   depths.forEach(function(row,index){
-//     div.innerHTML +=
-//     "<li style=\"background-color: " + colorscale[index] + "\"></li>" + row;
-//     return div;
-//   });
-  
-//  legend.addTo(myMap);
-// };
-
-
-
-//   for (var i = 0; i <depths.length; i++) {
-//     div.innerHTML += 
-//     "<li style=\"background-color: " + colorscale[i]+ "\">" + depths[i] + (depths[i+1] + "</li>" ? "&ndash;" + depths[i+1] + "<br>" : "+");
-//     //"<ul class =\"no-bullets\">" + "<li style=\"background-color: " + colorscale[i]+ "\"></li>" + depths[i] + (depths[i+1] ? "&ndash;" + depths[i+1] + "<br>" : "+" + "</ul>");
-//   }
-//   return div;
-// };
-// legend.addTo(myMap);
-
+})
 
